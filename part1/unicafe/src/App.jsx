6b64475e1d06c45472bdac250fecdbc140ca17e8
Leonardo Flores <florees.leo@gmail.com>
2024-10-10 14:22:56 -0600

    feat: :sparkles: part1 unicafe 1.6 paso 1
    Se da solución al ejercicio 1.6 de la parte 1, creando un componente Button para customizar la acción del botón y el texto del mismo, y un segundo componente para mostrar los numeros de los comentarios.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,3 +1,46 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+const Button = ({ action, text }) => {
+  return <button onClick={action}>{text}</button>;
+};
+
+const ComnentStats = ({ name, num }) => (
+  <div>
+    {name}: {num}
+  </div>
+);
+
 export default function App() {
-  return <div>App</div>;
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onClickGood = () => {
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+  };
+
+  const onClickNeutral = () => {
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+  };
+
+  const onClickBad = () => {
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+  };
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button action={onClickGood} text={"Good"} />
+      <Button action={onClickNeutral} text={"Nuetral"} />
+      <Button action={onClickBad} text={"Bad"} />
+      <h2>Staticts</h2>
+      <ComnentStats name={"Good"} num={good} />
+      <ComnentStats name={"Neutral"} num={neutral} />
+      <ComnentStats name={"Bad"} num={bad} />
+    </div>
+  );
 }
