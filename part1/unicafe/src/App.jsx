04920b50b6f7057705030da75ace19ae06c9e805
Leonardo Flores <florees.leo@gmail.com>
2024-10-10 16:40:00 -0600

    feat: :sparkles: part1 unicafe 1.7 paso 2
    Se modifica el código para que muestre más estadísticas sobre los comentarios recopilados, como el número total de comentarios recopilados, la puntuación promedio y el porcentaje de comentarios positivos.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Button = ({ action, text }) => {
   return <button onClick={action}>{text}</button>;
 };
 
-const ComnentStats = ({ name, num }) => (
+const CommentStats = ({ name, num }) => (
   <div>
     {name}: {num}
   </div>
@@ -15,20 +15,33 @@ export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+
+  useEffect(() => {
+    const calcAverage = ((good + neutral + bad) / 3).toFixed(4);
+    setAverage(calcAverage);
+  }, [all]);
 
   const onClickGood = () => {
     const updatedGood = good + 1;
+    const updatedAll = all + 1;
     setGood(updatedGood);
+    setAll(updatedAll);
   };
 
   const onClickNeutral = () => {
     const updatedNeutral = neutral + 1;
+    const updatedAll = all + 1;
     setNeutral(updatedNeutral);
+    setAll(updatedAll);
   };
 
   const onClickBad = () => {
     const updatedBad = bad + 1;
+    const updatedAll = all + 1;
     setBad(updatedBad);
+    setAll(updatedAll);
   };
 
   return (
@@ -37,10 +50,16 @@ export default function App() {
       <Button action={onClickGood} text={"Good"} />
       <Button action={onClickNeutral} text={"Nuetral"} />
       <Button action={onClickBad} text={"Bad"} />
-      <h2>Staticts</h2>
-      <ComnentStats name={"Good"} num={good} />
-      <ComnentStats name={"Neutral"} num={neutral} />
-      <ComnentStats name={"Bad"} num={bad} />
+      <h2>Statistics</h2>
+      <CommentStats name={"Good"} num={good} />
+      <CommentStats name={"Neutral"} num={neutral} />
+      <CommentStats name={"Bad"} num={bad} />
+      <CommentStats name={"All"} num={all} />
+      <CommentStats name={"Average"} num={average} />
+      <CommentStats
+        name={"Positive"}
+        num={((good / all) * 100).toFixed(2) + "%"}
+      />
     </div>
   );
 }
