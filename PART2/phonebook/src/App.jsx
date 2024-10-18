dd65a1a86bea16fa7abf3ddbde06f41a68fed989
Leonardo Flores <florees.leo@gmail.com>
2024-10-18 12:56:33 -0600

    feat: :white_check_mark: PART2 phonebook 2.6
    Se hace funcional el formulario, añadiendole una función de flecha para añadir una nueva persona al arreglo de personas.
    
    Se hace un mapeo del arreglo persons para mostrar las personas.

--- PART2/phonebook/src/App.jsx
+++ PART2/phonebook/src/App.jsx
@@ -4,19 +4,32 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addPerson = (event) => {
+    event.preventDefault();
+    setPersons([...persons, { name: newName }]);
+    setNewName("");
+    // console.log(persons);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name:{" "}
+          <input
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   );
 };
