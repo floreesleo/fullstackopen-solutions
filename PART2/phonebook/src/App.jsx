e5c1f718a50a1220606711deaafe169134b0fbaf
Leonardo Flores <florees.leo@gmail.com>
2024-10-19 16:03:44 -0600

    feat: :white_check_mark: PART2 phonebook 2.9
    Se implementa un campo de búsqueda que puede usarse para filtrar la lista de personas por nombre y por número, por medio del método filter().

--- PART2/phonebook/src/App.jsx
+++ PART2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterText, setFilter] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -32,10 +36,24 @@ const App = () => {
     console.log("After creating new person: ", newPersons);
   };
 
+  const filteredPersons = persons.filter(
+    (person) =>
+      person.name.toLowerCase().includes(filterText.toLowerCase()) ||
+      person.number.includes(filterText)
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input
+          value={filterText}
+          onChange={(event) => setFilter(event.target.value)}
+        />
+      </div>
       <form onSubmit={addPerson}>
+        <h2>add a new</h2>
         <div>
           name:{" "}
           <input
@@ -55,10 +73,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>
-          {person.name} {person.number}
-        </p>
+      {filteredPersons.map((person) => (
+        <div key={person.id}>
+          {person.name} - {person.number}
+        </div>
       ))}
     </div>
   );
