786dbd6699014d5e5f4153c512b112e200bf3afa
Leonardo Flores <florees.leo@gmail.com>
2024-10-21 14:18:21 -0600

    feat: :sparkles: Implementación de axios
    Se implementa axios para obtener obtener las notas del servidor json-server, para esto, se hace uso de useEffect, para hacer la petición al servidor cuando se renderice el componente App.

--- EXAMPLES/notes/src/App.jsx
+++ EXAMPLES/notes/src/App.jsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function App() {
+  const [notes, setNotes] = useState([]);
+
+  const hook = () => {
+    console.log("Effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fullfilled");
+      setNotes(response.data);
+    });
+  };
+
+  useEffect(hook, []);
+  console.log("render", notes.length, "notes");
+
   return <div>App</div>;
 }
