9dd405615a0ba3d968d7184986ae32e412127d43
Leonardo Flores <florees.leo@gmail.com>
2024-12-02 10:48:18 -0600

    feat: ✅ Express.static
    Se le indica a express que trabajara con archivos estáticos, específicamente con la carpeta dist

--- EXAMPLES/express/index.js
+++ EXAMPLES/express/index.js
@@ -5,9 +5,10 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(morgan("tiny"));
 app.use(cors());
+app.use(morgan("tiny"));
 app.use(express.json());
+app.use(express.static("dist"));
 
 let notes = [
   {
