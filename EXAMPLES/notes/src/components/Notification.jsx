ade8790c268c1d2d285689b64060b4b39e9f44e8
Leonardo Flores <florees.leo@gmail.com>
2024-12-03 14:32:41 -0600

    fix: ⚠️ Proyectos EXAMPLES | Notes removidos
    Se remueven los proyectos notes-express-backend y notes-frontend del repositorio del curso de Fullstack para trabajar mejor con la organización de las carpetas.
    
    Además, lidiando con los problemas de repositorios con la plataforma Render.

--- EXAMPLES/notes/src/components/Notification.jsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable react/prop-types */
-import React from "react";
-
-export default function Notification({ message }) {
-  if (message === null) return null;
-  return <div className="error">{message}</div>;
-}
