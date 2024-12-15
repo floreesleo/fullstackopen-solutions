ef311abb232c1a3b0726f14194c9a89d61fe24e4
Leonardo Flores <florees.leo@gmail.com>
2024-12-15 15:17:40 -0600

    fix: ✅ Proxy con Backend
    Se cambia la url para comunicarse con el backend en el archivo persons.js
    
    Se implementa un proxy al archivo vite.config.js para que se pueda comunicar el frontend con el backend.

--- PART2/phonebook/src/services/persons.js
+++ PART2/phonebook/src/services/persons.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
